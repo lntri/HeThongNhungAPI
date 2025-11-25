9c3c88099e8e85f323e4772cf5286a7b4a500686
Tri Le <lengoctri.it.92@gmail.com>
2025-11-25 23:54:56 +0700

    HeThongNhungAPI

--- app.py
+++ app.py
@@ -144,5 +144,5 @@ def home():
     return "Server is running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
     
